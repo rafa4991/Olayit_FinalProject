0c43a705d53ab423bb657a65777f6090c01045ae
rafa4991 <rafa.perez720@gmail.com>
2016-05-16 09:21:26 -0700

    Search Functionality Completed

--- app/controllers/product_list_controller.rb
+++ app/controllers/product_list_controller.rb
@@ -1,9 +1,52 @@
 class ProductListController < ApplicationController
+	#helper_method :sort_column, :sort_direction
+
 	def index
 		@products = Product.all
-	end
-	
-	def product_list
+
+
+		#Logic for the simple SearchBar
+		@order="asc"
+
+    	if !params[:search].nil?
+	     	
+		   	@searchinput = params[:search]
+		   	@searchcriteria="%#{params[:search]}%" 
+	      	@products = Product.where("name like ?",@searchcriteria)
+	    end
+
+	    #Logic for advanced options
+	    if !params[:order].nil?
+	    	@order = params[:order]
+		   	@ordercriteria="%#{params[:order]}%"
+		   	if @ordercriteria == "asc"
+		   		
+		   	else
+
+		   	end
+	    end
+	    if !params[:sort].nil?
+	    	@sort = params[:sort]
+		   	@sortcriteria="#{params[:sort]}"
+
+	    end
+	    if !params[:filter].nil?
+	    	@filter = params[:filter]
+		   	@filtercriteria="#{params[:filter]}"
+	    end
+
+    	if !params[:order].nil? && !params[:sort].nil? && !params[:filter].nil?
+
+    			#@products = Product.where("name = ? AND locked = ?", params[:orders], false)
+    			@products = Product.order(@sortcriteria).limit(@filtercriteria)
+
+    			#.where("orders_count = ? AND locked = ?", params[:orders], false)
+    			#@products = Product.find(:all, :order => @sortcriteria, :limit => 100)
+    	else
+
+	      		
+	    end
+
 	end
 
 end
