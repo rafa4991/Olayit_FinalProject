9871b5bc92db8d4176b5e761e2ebf13d1d4e7cbe
Raul <rvasqz86@gmail.com>
2016-06-02 10:13:02 -0500

    Added Backend To Delete Users, If we decide to do it.

--- app/controllers/clinical_users_controller.rb
+++ app/controllers/clinical_users_controller.rb
@@ -2,6 +2,7 @@ class ClinicalUsersController < ApplicationController
 
   before_action :logged_in_user, only: [:edit, :update]
   before_action :correct_user,   only: [:edit, :update]
+  before_action :admin_user,     only: :destroy
 
   def new
     @clinicaluser = ClinicalUser.new
@@ -38,6 +39,12 @@ class ClinicalUsersController < ApplicationController
     end
   end
 
+  def destroy
+    ClinicalUser.find(params[:id]).destroy
+    flash[:success] = "User deleted"
+    redirect_to users_url
+  end
+
   private
 
     def clinicaluser_params
@@ -59,5 +66,9 @@ class ClinicalUsersController < ApplicationController
       @user = ClinicalUser.find(params[:id])
       redirect_to(root_url) unless current_user?(@user)
    end
+
+   def admin_user
+      redirect_to(root_url) unless current_user.admin?
+    end
 end
 
--- app/controllers/vendor_users_controller.rb
+++ app/controllers/vendor_users_controller.rb
@@ -2,6 +2,7 @@ class VendorUsersController < ApplicationController
 
   before_action :logged_in_user, only: [:show, :edit, :update]
   before_action :correct_user,   only: [:show, :edit, :update]
+  before_action :admin_user,     only: :destroy
 
   def new
     @vendor = VendorUser.new
@@ -37,6 +38,12 @@ class VendorUsersController < ApplicationController
     end
   end
 
+  def destroy
+    VendorUser.find(params[:id]).destroy
+    flash[:success] = "User deleted"
+    redirect_to users_url
+  end
+
   private
 
     def user_params
@@ -56,5 +63,9 @@ class VendorUsersController < ApplicationController
     def correct_user
       @user = VendorUser.find(params[:id])
       redirect_to(root_url) unless current_user?(@user)
+    end
+
+    def admin_user
+      redirect_to(root_url) unless current_user.admin?
     end
 end
