4e412da6fac259dbef4d7b3fa5cabe053bbf2cd9
rafa4991 <rafa.perez720@gmail.com>
2016-06-02 22:13:07 -0700

    Price Filter Complete
    It works but is very shitty functionality. Trying to figure out a way to
    stack filters.

--- app/controllers/product_list_controller.rb
+++ app/controllers/product_list_controller.rb
@@ -42,17 +42,44 @@ class ProductListController < ApplicationController
 
     			#.where("orders_count = ? AND locked = ?", params[:orders], false)
     			#@products = Product.find(:all, :order => @sortcriteria, :limit => 100)
-    	else
-
-	      		
 	    end
+
+	    ##############SIDEBAR FILTER LOGIC######################
+
+	    if !params[:priceRange].nil?
+			priceRange = params[:priceRange].to_s
+			high = 0
+			low = 0
+			if priceRange == "1"
+				high = 1000
+			elsif priceRange == "2"
+				high = 2000
+				low = 1000
+			elsif priceRange == "3"
+				high = 3000
+				low =2000
+			elsif priceRange == "4"
+				high = 4000
+				low = 3000
+			elsif priceRange == "5"
+				high = 5000
+				low = 4000
+			else
+				low = 5000
+			end 
+
+			if high == 0 && low != 0
+				@products = @products.where("price >= ?", low)
+			else
+				@products = @products.where("price <= ? AND price >= ?", high, low)
+			end
+			
+		end
 
 	end
 
 	def filter
-		if !params[:priceRange].nil?
-			@priceRange = params[:priceRange]
-		end
+
 
 	end
 
